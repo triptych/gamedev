4936213f02671e910bf65e818862fb7b074a9359
Andrew Wooldridge <triptych@whisperstorm.com>
2018-03-04 16:47:52 +0000

    getting tiles to render

--- ittybittyrpg/scripts/ittybittyrpg.js
+++ ittybittyrpg/scripts/ittybittyrpg.js
@@ -7,6 +7,7 @@ var ibrpg = {
         console.log("this", this);
         
         this.initCanvas();
+        this.xx_renderOneTile();
     },
     state: {
         version: "0.01",
@@ -27,6 +28,7 @@ var ibrpg = {
         console.log("dom canvas",ds.cv)
         ctx = ds.ctx = ds.cv.getContext("2d");
         console.log(ds.ctx);
+        /**
         ctx.beginPath();
         ctx.rect(20, 20, 150, 100);
         ctx.fillStyle = "red";
@@ -36,6 +38,28 @@ var ibrpg = {
         ctx.rect(40, 40, 150, 100);
         ctx.fillStyle = "blue";
         ctx.fill();
+        **/
+        /** initially fill with black **/
+        ctx.beginPath();
+        ctx.rect(0,0,12*32,12*32);
+        ctx.fillStyle = "black";
+        ctx.fill();
+    },
+    xx_renderOneTile: function(){
+        console.log("in xx_renderOneTile");
+        var tileset = document.getElementById("dungeon");
+        console.log("tileset:", tileset);
+        var ds = this.datastore;
+        ds.ctx.drawImage(tileset, 0, 0, 32, 32);
+    },
+    xx_renderLayer: function(){
+        var tileset = document.getElementById("dungeon");
+        var ds = this.datastore;
+        for(var i=0; i<12; i++){
+            for(var j=0; j<12; j++){
+                ds.ctx.drawImage(tileset, 0,0, 32,32, i*32, j*32, 32,32);
+            }
+        }
         
     }
 }
@@ -45,3 +69,9 @@ window.addEventListener("DOMContentLoaded", function(e){
     console.log("addEventListener:", e);
     ibrpg.init();
 });
+
+window.addEventListener("load", function(e){
+    console.log("addEventListener", e);
+    //ibrpg.xx_renderOneTile();
+    ibrpg.xx_renderLayer();
+})
