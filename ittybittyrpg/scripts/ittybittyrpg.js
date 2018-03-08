37403b1f4bf84eea94ebb14bf1634a6217b18208
Andrew Wooldridge <triptych@whisperstorm.com>
2018-03-08 04:26:17 +0000

    adding a character

--- ittybittyrpg/scripts/ittybittyrpg.js
+++ ittybittyrpg/scripts/ittybittyrpg.js
@@ -9,8 +9,8 @@ const Constants = {
 }
 var ibrpg = {
     init: function(){
-        console.log("hello world");
-        console.log("this", this);
+        // console.log("hello world");
+        // console.log("this", this);
         
         this.initCanvas();
         this.xx_renderOneTile();
@@ -46,6 +46,16 @@ var ibrpg = {
               yOffset: 15
           }
       },
+      character: {
+        xOffset: 1,
+        yOffset: 2,
+        type: "dungeoncrawl",
+        name: "character",
+        pos: {
+            x: 2,
+            y: 2
+        }
+      },
       map:`w,w,w,w,w,w,w,w,w,w,w,w,
            w,f,f,f,f,f,f,f,f,f,f,w,
            w,f,f,f,f,f,f,f,f,f,f,w,
@@ -64,8 +74,8 @@ var ibrpg = {
         var ctx;
         //.log("ds:", ds);
         ds.cv = document.getElementById("ittybittyrpg");
-        ds.cv.setAttribute("width", 12 * 32 + "px");
-        ds.cv.setAttribute("height", 12 * 32 + "px");
+        ds.cv.setAttribute("width", Constants.WIDTH * Constants.SPWIDTH + "px");
+        ds.cv.setAttribute("height", Constants.HEIGHT * Constants.SPHEIGHT + "px");
         //console.log("dom canvas",ds.cv)
         ctx = ds.ctx = ds.cv.getContext("2d");
         //console.log(ds.ctx);
@@ -81,12 +91,18 @@ var ibrpg = {
         ctx.fill();
         **/
         /** initially fill with black **/
-        ctx.beginPath();
-        ctx.rect(0,0,12*32,12*32);
-        ctx.fillStyle = "black";
-        ctx.fill();
+        // ctx.beginPath();
+        // ctx.rect(0,0,12*32,12*32);
+        // ctx.fillStyle = "black";
+        // ctx.fill();
+        this.clearCanvas();
     },
     clearCanvas: function(){
+        var ctx = this.datastore.ctx;
+        ctx.beginPath();
+        ctx.rect(0,0,Constants.WIDTH * Constants.SPWIDTH, Constants.HEIGHT * Constants.SPHEIGHT);
+        ctx.fillStyle = "black";
+        ctx.fill();
         
     },
     xx_renderOneTile: function(){
@@ -94,7 +110,7 @@ var ibrpg = {
         var tileset = document.getElementById("dungeon");
         //console.log("tileset:", tileset);
         var ds = this.datastore;
-        ds.ctx.drawImage(tileset, 0, 0, 32, 32);
+        ds.ctx.drawImage(tileset, 0, 0, Constants.SPHEIGHT, Constants.SPWIDTH);
     },
     xx_renderLayer: function(){
         var tileset = document.getElementById("dungeoncrawl");
@@ -102,14 +118,14 @@ var ibrpg = {
         var ds = this.datastore;
         var dgcHeightInTiles = 64;
         var dgcWidthInTiles = 48;
-        var randomX = Math.floor(Math.random() * dgcWidthInTiles) *32;
-        var randomY = Math.floor(Math.random() * dgcHeightInTiles) *32;
+        var randomX = Math.floor(Math.random() * dgcWidthInTiles) * Constants.SPWIDTH;
+        var randomY = Math.floor(Math.random() * dgcHeightInTiles) * Constants.SPHEIGHT;
         //console.log("randomx", randomX)
         //console.log("randomY", randomY)
         this.initCanvas();
         for(var i=0; i<12; i++){
             for(var j=0; j<12; j++){
-                ds.ctx.drawImage(tileset, Math.floor(Math.random() * dgcWidthInTiles) *32 ,Math.floor(Math.random() * dgcHeightInTiles) *32, 32,32, i*32, j*32, 32,32);
+                ds.ctx.drawImage(tileset, Math.floor(Math.random() * dgcWidthInTiles) * Constants.SPWIDTH ,Math.floor(Math.random() * dgcHeightInTiles) * Constants.SPHEIGHT, 32,32, i*32, j*32, 32,32);
                 //console.log()
             }
         }
@@ -129,6 +145,23 @@ var ibrpg = {
         // ibrpg.xx_renderLayer();
         // window.requestAnimationFrame(ibrpg.xx_timedUpdate)
     },
+    xx_renderChar: function(){
+        var ds = this.datastore;
+        var ctx = ds.ctx;
+        var char = this.world.character;
+        
+        var tileset = document.getElementById(char.type); 
+        ctx.drawImage(tileset, 
+            char.xOffset * Constants.SPWIDTH,
+            char.yOffset * Constants.SPHEIGHT,
+            Constants.SPWIDTH,
+            Constants.SPHEIGHT,
+            char.pos.x * Constants.SPWIDTH,
+            char.pos.y * Constants.SPWIDTH,
+            Constants.SPWIDTH,
+            Constants.SPHEIGHT);
+        
+    },
     xx_renderLevel: function(){
         // assuming level is loaded
         var ds = this.datastore;
@@ -138,20 +171,20 @@ var ibrpg = {
         var tileDef = this.world.tileDef;
         console.log("mapArr:", mapArr);
         
-        for(var i=0; i<12; i++){
-            for(var j=0; j<12; j++){
+        for(var i = 0; i < Constants.WIDTH; i++){
+            for(var j = 0; j < Constants.HEIGHT; j++){
                // ds.ctx.drawImage(tileset, Math.floor(Math.random() * dgcWidthInTiles) *32 ,Math.floor(Math.random() * dgcHeightInTiles) *32, 32,32, i*32, j*32, 32,32);
-              console.log("letter:", mapArr[(i*12) + j].trim());
-              console.log("tile: ", tileDef[mapArr[(i*12) + j].trim()]);
-              ds.ctx.drawImage(tileset, 
-                tileDef[mapArr[(i*12) + j].trim()].xOffset * 32, 
-                tileDef[mapArr[(i*12) + j].trim()].yOffset * 32,
-                32,
-                32,
-                j * 32,
-                i * 32,
-                32,
-                32)
+              //console.log("letter:", mapArr[(i*12) + j].trim());
+            //   console.log("tile: ", tileDef[mapArr[(i*12) + j].trim()]);
+              ctx.drawImage(tileset, 
+                tileDef[mapArr[(i*Constants.WIDTH) + j].trim()].xOffset * Constants.SPWIDTH, 
+                tileDef[mapArr[(i*Constants.HEIGHT) + j].trim()].yOffset * Constants.SPHEIGHT,
+                Constants.SPWIDTH,
+                Constants.SPHEIGHT,
+                j * Constants.SPWIDTH,
+                i * Constants.SPHEIGHT,
+                Constants.SPWIDTH,
+                Constants.SPHEIGHT)
                
             }
         }
@@ -179,5 +212,23 @@ window.addEventListener("keydown", function(e){
     console.log("keydown");
     const keyName = event.key;
     console.log('keydown event\n\n' + 'key: ' + keyName);
-})
+    
+    
+    if(keyName == "ArrowRight"){
+        ibrpg.world.character.pos.x+=1;
+    }
+        if(keyName == "ArrowLeft"){
+        ibrpg.world.character.pos.x-=1;
+    }
+        if(keyName == "ArrowUp"){
+        ibrpg.world.character.pos.y-=1;
+    }
+        if(keyName == "ArrowDown"){
+        ibrpg.world.character.pos.y+=1;
+    }
+    
+    ibrpg.xx_renderLevel();
+    ibrpg.xx_renderChar();
+    
+});
 
